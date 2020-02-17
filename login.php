ea0a387b09602d07a7ec3164adb50fc81e46e9f9
Alexanderanger <56217904+Alexanderanger@users.noreply.github.com>
2020-02-17 10:20:47 +0100

    Create login.php
    test

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+include("header.php");
+?>
+
+
+
+
+<form action="handlelogin.php" method="post">
+
+    <input type="text" placeholder="Username.." name="username" />
+    <input type="password" placeholder="Password.." name="password" />
+
+</form>
+
+
+
+
+
+
+<?php
+include("footer.php");
+?>
